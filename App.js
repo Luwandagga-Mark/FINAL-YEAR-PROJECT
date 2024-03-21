e5bbe41336bf578dfbcebcb8a0383a712614c7d0
Luwandagga Mark <markluiz60@gmail.com>
2024-03-21 12:51:21 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import SignInScreen from './src/screens/SignInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+import Guardian from './src/screens/Guardian';
+import Services from './src/screens/Services';
+import Others from './src/screens/OTHERS/Others';
+import Accept from './src/screens/Accept/Accept';
+import Denied from './src/screens/Denied/Denied';
 
-export default function App() {
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <SignUpScreen />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F9FBFC'
   },
 });
+export default App;
