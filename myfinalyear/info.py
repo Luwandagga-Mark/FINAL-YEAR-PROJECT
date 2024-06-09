db84dfd486439c08ff25f7d377c68cc73cf048b8
mugumbyabenon <legacyallan0@gmail.com>
2024-06-09 22:24:58 +0300

    backend functioning

--- myfinalyear/info.py
+++ myfinalyear/info.py
@@ -1,6 +1,9 @@
+localcode = 'gaxgrkbmopbeltix'
+online= 'mhbwklswwykyehek'
 EMAIL_FROM= 'EMAIL_FROM_USER'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER='makereremanifest@gmail.com'
-EMAIL_HOST_PASSWORD = 'gaxgrkbmopbeltix'
+EMAIL_HOST_PASSWORD = localcode
 EMAIL_USE_TLS = True
 EMAIL_PORT= 587
+
