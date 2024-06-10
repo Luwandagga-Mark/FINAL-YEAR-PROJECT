81ac9edf90a90eec7f0fb7bb614714a1d21696d6
mugumbyabenon <legacyallan0@gmail.com>
2024-06-10 08:17:50 +0300

    checks

--- myfinalyear/info.py
+++ myfinalyear/info.py
@@ -3,7 +3,7 @@ online= 'mhbwklswwykyehek'
 EMAIL_FROM= 'EMAIL_FROM_USER'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_HOST_USER='makereremanifest@gmail.com'
-EMAIL_HOST_PASSWORD = localcode
+EMAIL_HOST_PASSWORD = online
 EMAIL_USE_TLS = True
 EMAIL_PORT= 587
 
